1ffcbd8a497253c58a02b7aac4fe9135326220ad
Felipe Montefuscolo <felipe.mt87@gmail.com>
2012-11-28 08:30:41 -0200

    add two examples

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -2,11 +2,56 @@
 #define EAD_DEBUG       // to debug
 #include "Ead/ead.hpp"
 
-// x.val() = ..   set the value
-// x       = ..   set the value set to zero all x components
+
+// A very simple example with polynomials
+
+
+// A define for syntax sugar.
+// 10 is the maximum num of components, not the actual size
+typedef ead::DFad<double, 10> adouble;
+
 
 int main()
 {
+  const double pi = 3.14159265;
   
+  int const deg = 3; // polynomial degree
+  
+  double a[deg+1]; // Polynomail coefficients. As they ar constantes, they
+                 // can be double.
+  
+  // set coefficients values
+  for (int i = 0; i < deg+1; ++i)
+    a[i] = 1.0;
+  
+  
+  adouble x,y;
+
+  x.setDiff(0, 1); // first argument set x as an zeroth independent variable
+                   // second argument says that the problem has one independent variable
+  
+  x.val() = pi;    // the polynomail will be evaluated at x = pi
+                   // NOTE: do not use x = pi ... this way you make x a constant
+  
+  y.setDiff(-1, 1); // first argument set y as an dependent variable
+                    // second argument says that y has one independent variable
+
+
+  // ----- compute the polynomial
+  y = a[deg];
+  for (int i = deg+2; i-- > 2 ;)
+    y = y*x + a[i-2];
+
+
+  // check the result
+  std::cout << "function value: \n";
+  std::cout << "* AD:    " << y.val() << std::endl;
+  std::cout << "* exact: " << a[0] + a[1]*x.val() + a[1]*pow(x.val(),2) +  a[1]*pow(x.val(),3) << std::endl << std::endl;
+  std::cout << "derivative value: \n";
+  std::cout << "* AD:    " << y.dx() << std::endl;
+  std::cout << "* exact: " << a[1] + 2*a[1]*x.val() +  3*a[1]*pow(x.val(),2) << std::endl << std::endl;
   
 }
+
+
+
---
+++ examples/example2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#define EAD_DEBUG       // to debug
+#include "Ead/ead.hpp"
+
+using namespace std;
+
+// def AD number type with (30 is the maximum num of components)
+typedef ead::DFad<double, 30> adouble;
+
+template<class Vec>
+void F1 (Vec const& x, Vec &y) {
+  y[0] = sin(x[0]);
+  y[1] = exp(2*x[0]*x[1]);
+  y[2] = sqrt(pow(x[0],2) + 2.*pow(x[1],2));
+}
+
+int main()
+{
+  int n_eqs = 3;
+  int n_unk = 2;
+
+  std::cout.setf(std::ios::scientific);
+  std::cout.precision(5);
+
+  vector<adouble> x(n_unk, adouble(0,n_unk)),
+                  y(n_eqs, adouble(0,n_unk)),
+                  dydx(n_unk*n_eqs, adouble(0,n_unk));
+  
+  for (int i = 0; i < n_unk; ++i)
+    x[i].setDiff(i, n_unk);
+
+  // x.val() = ..   set the value
+  // x       = ..   set the value set to zero all x components
+  x[0].val() = 1./3.;
+  x[1].val() = 8./3.;
+
+  // compute function y and your derivatives (automatically)
+  F1(x,y);
+
+  // print jacobian
+  std::cout << "\nJacobian computed by AD:\n\n";
+  for (int i = 0; i < n_eqs; ++i)
+  {
+    for (int j = 0; j < n_unk; ++j)
+      std::cout << y[i].dx(j) << " ";
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+  
+}
